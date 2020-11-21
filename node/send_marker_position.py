0bdf33f44b36d5f4f54136d1d0be1f44f2cb488d
PinkWink <pinkwink.korea@gmail.com>
2020-11-21 10:09:24 +0900

    bug_fix

--- node/send_marker_position.py
+++ node/send_marker_position.py
@@ -8,30 +8,42 @@ from time import sleep
 from ar_track_alvar_msgs.msg import AlvarMarkers
 
 baud_rate = 115200
-port = "/dev/ttyTHS1"
+port_num = "/dev/ttyUSB0"
 
-# prrospy.get_param('~port')
-
-ar_port = serial.Serial(port=port, baudrate=baud_rate)
+ar_port = serial.Serial(port=port_num, baudrate=baud_rate)
 
 time.sleep(0.5)
-ar_port.write("$READY\r")
-time.sleep(1)
+ar_port.write("$READY\r\n")
+print "$READY\r\n"
+time.sleep(0.5)
+
+def convert_id(id):
+    if id == 10:
+        marker_id = 'S'
+    elif id == 11:
+        marker_id = 'B'
+    elif id == 12:
+        marker_id = 'C'
+    elif id == 13:
+        marker_id = 'A'
+    else:
+        marker_id = str(id)
+
+    return marker_id
 
 def callback(msg):
     time.sleep(0.1)
-    wait_signal = "-"  + '\n\r'
-    print wait_signal
-    ar_port.write(wait_signal)
-    
+    time_stamp_signal = 'X'+'\r\n'
+    ar_port.write(time_stamp_signal)
+    print time_stamp_signal
+
     for each in msg.markers:
-        marker_id = int(each.id)
+        marker_id = convert_id(int(each.id))
         x_pos = int(each.pose.pose.position.x*1000)
         y_pos = int(each.pose.pose.position.y*1000)
-        # send_result = 'id:' + str(marker_id) + ', ' + 'x:' + str(x_pos) + ', ' + 'y:' + str(y_pos) + '\n\r'
-        send_result = "$"+str(marker_id)+","+str(x_pos)+","+str(y_pos)+'\n\r'
+        send_result = "$"+marker_id+","+str(x_pos)+","+str(y_pos)+'\r\n'
+
         print send_result
-        # time.sleep(0.1)
         ar_port.write(send_result)
 
 rospy.init_node('get_marker_position')
